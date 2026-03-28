d284560d5cd8599b2bab90b0c15d19ca79f693b7
FaridRieziq <94285977+FaridRieziq@users.noreply.github.com>
2026-03-28 23:39:48 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,21 @@
-let input = "";
+let currentScreen = 1;
 
 // Pindah screen
-function nextScreen(num) {
-  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
-  document.getElementById("screen" + num).classList.add("active");
-}
-
-
-// Musik
-function playMusic() {
-  const music = document.getElementById("music");
-  music.play().catch(() => {});
-}
-
-let currentScreen = 0;
-
 function nextScreen(num) {
   document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
   document.getElementById("screen" + num).classList.add("active");
   currentScreen = num;
 }
 
+// Kembali screen
 function prevScreen() {
-  if (currentScreen > 0) {
+  if (currentScreen > 1) {
     nextScreen(currentScreen - 1);
   }
+}
+
+// Musik
+function playMusic() {
+  const music = document.getElementById("music");
+  music.play().catch(() => {});
 }
